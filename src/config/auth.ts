98271e2ed8c1acb7681c4429fdec5ff69435f9bf
Jeniffer de Azevedo <jeniqf@gmail.com>
2020-06-17 18:47:50 +0100

    Att

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'f57f2b69aed9259c9b29b21d1c29bb43',
+    expiresIn: '1d',
+  },
+};
---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,40 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppError from '../errors/AppError';
+
+interface RequestDTO {
+  provider_id: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({
+    provider_id,
+    date,
+  }: RequestDTO): Promise<Appointment> {
+    const appointmentRepository = getCustomRepository(
+      AppointmentsRepository,
+    );
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
+
+    const appointment = appointmentRepository.create({
+      provider_id,
+      date: appointmentDate,
+    });
+
+    await appointmentRepository.save(appointment);
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
