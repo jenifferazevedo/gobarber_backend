7b87f7c1c2118748f97d6574ba7266e0966b51da
Jeniffer de Azevedo <jeniqf@gmail.com>
2020-06-17 22:42:36 +0100

    Changing the organization to Domain Driven Design

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,10 +2,10 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import '@shared/infra/typeorm';
 import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
-import './database';
 
 const app = express();
 app.use(cors());
